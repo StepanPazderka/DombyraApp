3c31ed36460b790c85acb285313f0eb9dd509103
Stepan Pazderka <stepan.pazderka@icloud.com>
2026-02-01 22:38:43 +0100

    Initial Commit

---
+++ DombyraApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DombyraApp
+//
+//  Created by Štěpán Pazderka on 01.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DombyraApp/DombyraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DombyraAppApp.swift
+//  DombyraApp
+//
+//  Created by Štěpán Pazderka on 01.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct DombyraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
